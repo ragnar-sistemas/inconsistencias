2b0c89063ed7128b746af5e102e70ba8e603c30e
alexa <alexa@ALEXANDRE>
2019-05-02 12:12:42 -0300

    componente de inconsistencia

---
+++ src/inconsistencia/Inconsistencia.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+import lupa from '../images/lupa.png'
+
+class Inconsistencia extends Component {
+	render() {
+		return (
+			<tr>
+				<td>
+					{this.props.id}
+				</td>
+				<td>
+					{this.props.mensagem}
+				</td>
+				<td>
+					<img src={lupa} className="img" alt="logo" width="20" height="20"/>
+				</td>
+			</tr>
+		)
+	}
+
+}
+
+export default Inconsistencia
--- src/inconsistencia/InconsistenciaList.js
+++ src/inconsistencia/InconsistenciaList.js
@@ -1,38 +1,37 @@
 import React, { Component } from 'react'
-import logo from '../images/lupa.png';
+
 import InconsistenciaData from '../dados/inconsistencias.json'
+import Inconsistencia from './Inconsistencia'
 
 class InconsistenciaList extends Component {
 
 	render(){
 		return (
 			<div className="container">
-				<h2>Inconsitencias Para Geração de Lançamentos</h2>
+				<h2>Inconsitências Para Geração de Lançamentos</h2>
 				<table className="table table-condensed">
 					<thead>
 				      <tr>
+				      	<th>Id</th>
 				        <th>Inconsitência</th>
+				        <th/>
 				      </tr>
 				    </thead>
 				    <tbody>
-				    	{InconsistenciaData.map((inconsistenciaDetalhe, index) => {
-							return <tr>
-								<td>
-									{inconsistenciaDetalhe.mensagem}
-								</td>
-								<td>
-									<img src={logo} className="img" alt="logo" width="20" height="20"/>
-								</td>
-							</tr>
-							})
+				    	{
+				    		InconsistenciaData.map(
+				    			(inconsistenciaDetalhe, index) => {
+									return <Inconsistencia 
+										id={inconsistenciaDetalhe.id} 
+										mensagem={inconsistenciaDetalhe.mensagem}/>
+								}
+							)
 						}
 				    </tbody>
-
 				</table>
 			</div>
 		)
 	}
-
 }
 
 export default InconsistenciaList
